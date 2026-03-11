d43968806746e2c9c1e0d696e07e3f3135071063
David Galvez <davidcap219@@gmail.com>
2026-03-11 15:22:36 +0100

    testeando los mensajes de kafka

--- src/test/java/com/example/demo/MultiThreadProjectApplicationTests.java
+++ src/test/java/com/example/demo/MultiThreadProjectApplicationTests.java
@@ -2,8 +2,13 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import com.main.MultiThreadProjectApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = MultiThreadProjectApplication.class, properties = {
+		"spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}"
+})
+@EmbeddedKafka(partitions = 1, topics = { "emails" })
 class MultiThreadProjectApplicationTests {
 
 	@Test
